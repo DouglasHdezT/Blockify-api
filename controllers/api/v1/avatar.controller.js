0f13dbbdbd62f97eb1e56a3a92914e7a536b17cb
DouglasHdezT <00004516@uca.edu.sv>
2020-10-05 21:58:15 -0600

    Fix: fs lib

--- controllers/api/v1/avatar.controller.js
+++ controllers/api/v1/avatar.controller.js
@@ -7,11 +7,15 @@ const host = process.env.HOST || "localhost:3000";
 
 controller.findAll = async (req, res) => {
     try {
-        const files = await fs.readdir(path.join(process.cwd(), "public/avatars"));
+        fs.readdir(path.join(process.cwd(), "public/avatars"), (err, files) => { 
+            if(err) return res.status(500).json({ error: "Internal Server Error" });
+            
+            const avatars = files.map(file => `${host}/avatars/${file}`);
+            
+            return res.status(200).json(avatars);
+        } );
 
-        const avatars = /* files.map(file => `${host}/avatars/${file}`) */[];
 
-        return res.status(200).json(avatars);
     } catch (error) {
         return res.status(500).json({ error: "Internal Server Error" });
     }
