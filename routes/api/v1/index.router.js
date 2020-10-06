389ab814d4453343a2a2b23de2fdce3af69ecc97
DouglasHdezT <00004516@uca.edu.sv>
2020-10-05 21:36:27 -0600

    Add: Avatars management

--- routes/api/v1/index.router.js
+++ routes/api/v1/index.router.js
@@ -4,9 +4,11 @@ const router = express.Router();
 //Require routers
 const userRouter = require("./user.router");
 const tagRouter = require('./tag.router');
+const avatarRouter = require("./avatar.router");
 
 //Register routes
 router.use("/user", userRouter);
+router.use("/avatar", avatarRouter);
 router.use('/tag', tagRouter);
 
 module.exports = router;
