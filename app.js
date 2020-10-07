07dd2adbb19fddd18ef80d7902968e877fc8d4fd
DouglasHdezT <00004516@uca.edu.sv>
2020-10-06 20:14:23 -0600

    Add: Cors support

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
 
 var indexRouter = require('./routes/index.router');
 const database = require("./config/database");
 
 var app = express();
 database.connect();
+
+app.use(cors());
 
 app.use(logger('dev'));
 app.use(express.json());
