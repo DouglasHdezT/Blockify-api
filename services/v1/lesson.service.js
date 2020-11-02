db079578ff4ab5135bbc7c73e3e8cbab31d6896f
DouglasHdezT <00004516@uca.edu.sv>
2020-11-02 00:17:24 -0600

    Add: Lesson service

--- services/v1/lesson.service.js
+++ services/v1/lesson.service.js
@@ -1,9 +1,11 @@
 const Lesson = require("@internal/models/Lesson");
 const ServiceResponse = require("@internal/classes/ServiceResponse");
 
+const { sanitizeObject } = require("@internal/utils/objects.tools");
+
 const service = {};
 
-service.create = async (title, content, creator, private = false) => { 
+service.create = async (title, content, creator, private = false) => {
     try {
         const lesson = new Lesson({
             title,
@@ -16,6 +18,62 @@ service.create = async (title, content, creator, private = false) => {
         if (!lessonSaved) return new ServiceResponse(false, { error: "Lesson not Created" });
         
         return new ServiceResponse(true, { message: "Lesson created!" });
+    } catch (error) {
+        throw error;
+    }
+};
+
+service.findAll = async () => { 
+    try {
+        const lessons = await Lesson.find({}) || [];
+        
+        return new ServiceResponse(true, lessons);
+    } catch (error) {
+        throw error;
+    }
+}
+
+service.findAllByUser = async (userID) => { 
+    try {
+        const lessons = await Lesson.find({ creator: userID }) || [];
+
+        return new ServiceResponse(true, lessons);
+    } catch (error) {
+        throw error;
+    }
+}
+
+service.findById = async (id) => { 
+    try {
+        const lesson = await Lesson.findById(id);
+        if (!lesson) return new ServiceResponse(false, { error: "Lesson not found" });
+
+        return new ServiceResponse(true, lesson);
+    } catch (error) {
+        throw error;
+    }
+}
+
+service.updateOneLesson = async (lessonID, { title, content, private }) => { 
+    try {
+        const sanitizedObject = sanitizeObject({ title, content, private });
+        const lessonUpdated = await Lesson.findByIdAndRemove(lessonID, { ...sanitizedObject });
+        
+        if (!lessonUpdated) return new ServiceResponse(false, { error: "Lesson didn't updated" });
+
+        return new ServiceResponse(true, { message: "Lesson updated!" });
+    } catch (error) {
+        throw error;
+    }
+}
+
+service.deleteById = async (lessonID) => { 
+    try {
+        const lessonDeleted = await Lesson.findByIdAndDelete(lessonID);
+
+        if (!lessonDeleted) return new ServiceResponse(false, { error: "Lesson didn't deleted" });
+        
+        return new ServiceResponse(true, { message: "Lesson deleted!" });
     } catch (error) {
         throw error;
     }
