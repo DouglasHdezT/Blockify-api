dfee3d703b209a1ae7db6bda2d277d237f0832b3
DouglasHdezT <00004516@uca.edu.sv>
2020-11-06 17:42:56 -0600

    Add: User Stars methods

--- controllers/api/v1/user.controller.js
+++ controllers/api/v1/user.controller.js
@@ -46,4 +46,87 @@ controller.delete = async (req, res) => {
     }
 }
 
+/**
+ * Rate methods
+ */
+
+controller.addRate = async (req, res) => { 
+    try {
+        const { _id: myUserID } = req.user;
+        const { userID, rate } = req.body;
+
+        const { status: userExists, content: user } = await userService.findOneById(userID);
+        if (!userExists) return res.status(404).json({ error: "User not found" });
+        
+        const alreadyRated = user.isUserInStars(myUserID);
+        if (alreadyRated) return res.status(409).json({ error: "Already rated!" });
+        
+        const { status: rateAdded } = await userService.addStar(user, myUserID, rate);
+        if (!rateAdded) return res.status(409).json({ error: "Cannot rate" });
+
+        return res.status(201).json({ message: "Rate added!" });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ error: "Internal Server Error" });
+    }
+}
+
+controller.getRate = async (req, res) => { 
+    try {
+        const { id } = req.params;
+        
+        const { status: userExists, content: user } = await userService.findOneById(id);
+        if (!userExists) return res.status(404).json({ error: "User not found" });
+
+        return res.status(200).json({
+            username: user.username,
+            rate: user.stars
+        });
+    } catch (error) {
+        return res.status(500).json({ error: "Internal Server Error" });
+    }
+}
+
+controller.deleteRate = async (req, res) => { 
+    try {
+        const { _id: myUserID } = req.user;
+        const { userID } = req.body;
+
+        const { status: userExists, content: user } = await userService.findOneById(userID);
+        if (!userExists) return res.status(404).json({ error: "User not found" });
+        
+        const alreadyRated = user.isUserInStars(myUserID);
+        if (!alreadyRated) return res.status(409).json({ error: "didn't rated!" });
+
+        const { status: rateDeleted } = await userService.deleteStar(user, myUserID);
+        if (!rateDeleted) return res.status(409).json({ error: "Cannot delete rate" });
+        
+        return res.status(200).json({ message: "Rate deleted" });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ error: "Internal server error" });
+    }
+}
+
+controller.updateRate = async (req, res) => { 
+    try {
+        const { _id: myUserID } = req.user;
+        const { userID, rate } = req.body;
+
+        const { status: userExists, content: user } = await userService.findOneById(userID);
+        if (!userExists) return res.status(404).json({ error: "User not found" });
+        
+        const alreadyRated = user.isUserInStars(myUserID);
+        if (!alreadyRated) return res.status(409).json({ error: "didn't rated!" });
+
+        const { status: rateUpdated } = await userService.updateStar(user, myUserID, rate);
+        if (!rateUpdated) return res.status(409).json({ error: "Cannot update rate" });
+        
+        return res.status(200).json({ message: "Rate Updated" });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ error: "Internal server error" });
+    }
+}
+
 module.exports = controller;
---
+++ validators/v1/user.validator.js
@@ -0,0 +1,26 @@
+const { check, param } = require("express-validator");
+
+const validator = {};
+
+validator.addRateValidator = [
+    check("userID")
+        .notEmpty().withMessage("UserID is required!")
+        .isMongoId().withMessage("userID must be an Mongo ID"),
+    check("rate")
+        .notEmpty().withMessage("Rate is required!")
+        .isFloat({ min: 0, max: 5 }).withMessage("Rate must be number and between 0 and 5"),
+];
+
+validator.deleteRateValidator = [
+    check("userID")
+        .notEmpty().withMessage("UserID is required!")
+        .isMongoId().withMessage("userID must be an Mongo ID"),
+];
+
+validator.idInParamsValidator = [
+    param("id")
+        .notEmpty().withMessage("UserID is required!")
+        .isMongoId().withMessage("userID must be an Mongo ID"),
+];
+
+module.exports = validator;
